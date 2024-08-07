181f57f189bb434253178f6674f7dfa1263858be
jyw19941 <hday19941@naver.com>
2024-08-07 17:04:14 +0900

    coding file

---
+++ web0806_mybatis/src/main/java/drive/TestMain.java
@@ -0,0 +1,28 @@
+package drive;
+
+import java.util.List;
+
+import dept.DeptDAO;
+import dept.DeptDTO;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DeptDAO dao = new DeptDAO();
+		int deptno = dao.searchDeptno("SALES");
+		System.out.println(deptno);
+		List<DeptDTO> list = dao.listDept();
+		for(DeptDTO dto : list) {
+			System.out.println(dto);
+		}
+		System.out.println("--------------------");
+		DeptDTO dto = dao.searchLoc("BOSTON");
+		System.out.println(dto);
+	}
+
+}
+
+
+
+
